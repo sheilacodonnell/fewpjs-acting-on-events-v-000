d12d454b9e7fa03d7b2d1ab64c0c9fd04797f40d
Sheila O'Donnell <sheila@contexttravel.com>
2020-03-30 14:46:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function moveDodgerRight() {
   let right = parseInt(rightNumbers, 10);
  
   if (right > 0) {
-    dodger.style.right = `${right + 1}px`;
+    dodger.style.right = `${left + 1}px`;
   }
 }
 
